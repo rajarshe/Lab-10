08363c17fe75aee1950dca95a69650ed4c795dd7
Rajarshe <rajarshe@umich.edu>
2023-11-30 20:01:50 -0500

    branch

--- textSelectionSort.java
+++ textSelectionSort.java
@@ -24,74 +24,7 @@ public class testSelectionSort{
 
       }
 
-      private void testPositive() {
-
-            int[] arr = new int[5];
-
-            arr[0] = 8;
-
-            arr[1] = 9;
-
-            arr[2] = 7;
-
-            arr[3] = 10;
-
-            arr[4] = 2;
-           
-
-            int[] Sortedarr = new int[5];
-
-            Sortedarr[0] = 2;
-
-            Sortedarr[1] = 7;
-
-            Sortedarr[2] = 8;
-
-            Sortedarr[3] = 9;
-
-            Sortedarr[4] = 10;
-
-           
-
-            SelectionSort SelectionSort = new SelectionSort ();
-            System.out.print ("Sorted Positive: ");
-            
-      }
-
-      private void testNegative() {
-
-            int[] arr = new int[5];
-
-            arr[0] = -8;
-
-            arr[1] = -9;
-
-            arr[2] = -7;
-
-            arr[3] = -10;
-
-            arr[4] = -2;
-
-           
-
-            int[] Sortedarr = new int[5];
-
-            Sortedarr[0] = -10;
-
-            Sortedarr[1] = -9;
-
-            Sortedarr[2] = -8;
-
-            Sortedarr[3] = -7;
-
-            Sortedarr[4] = -2;
-
-           
-
-            SelectionSort SelectionSort = new SelectionSort ();
-
-      }
-
+      
       private void testMixed() {
 
             int[] arr = new int[5];
